ea67cb9354cc33326e7d9d597c07f6aacbdbe82f
blagowhatnow <58713481+blagowhatnow@users.noreply.github.com>
2023-06-30 21:52:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,22 @@ from rdkit.Chem import rdFreeSASA
 from rdkit.Chem import Draw
 import SessionState
 
+def filter_valid_smiles(csv_file):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+
+    # Check validity of SMILES in each row
+    valid_smiles = []
+    for smiles in df["Ligand SMILES"]:
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is not None:
+            valid_smiles.append(smiles)
+
+    # Filter the DataFrame to include only rows with valid SMILES
+    filtered_df = df[df["Ligand SMILES"].isin(valid_smiles)].reset_index(drop=True)
+
+    return filtered_df
+
 def calculate_sasa(mol):
     try:
         hmol = Chem.AddHs(mol)
@@ -19,11 +35,9 @@ def calculate_sasa(mol):
         sasa = float('nan')
     return sasa
 
-
 def calculate_qed(mol):
     qed = Descriptors.qed(mol)
     return qed
-
 
 def generate(smiles, verbose=False):
     moldata = []
@@ -36,16 +50,16 @@ def generate(smiles, verbose=False):
         # Generate 3D coordinates
         AllChem.EmbedMolecule(mol)
 
-        desc_MolLogP = Descriptors.MolLogP(mol)
-        desc_MolWt = Descriptors.MolWt(mol)
-        desc_NumRotatableBonds = Descriptors.NumRotatableBonds(mol)
-        desc_HBONDAcceptors = Chem.Lipinski.NumHDonors(mol)
-        desc_HBONDDonors = Chem.Lipinski.NumHAcceptors(mol)
-        desc_TPSA = Descriptors.TPSA(mol)
-        desc_HeavyAtoms = Descriptors.HeavyAtomCount(mol)
-        desc_NumAromaticRings = Descriptors.NumAromaticRings(mol)
-        desc_QED = calculate_qed(mol)
-        desc_SASA = calculate_sasa(mol)
+        desc_MolLogP = round(Descriptors.MolLogP(mol), 3)
+        desc_MolWt = round(Descriptors.MolWt(mol), 3)
+        desc_NumRotatableBonds = round(Descriptors.NumRotatableBonds(mol), 3)
+        desc_HBONDAcceptors = round(Chem.Lipinski.NumHDonors(mol), 3)
+        desc_HBONDDonors = round(Chem.Lipinski.NumHAcceptors(mol), 3)
+        desc_TPSA = round(Descriptors.TPSA(mol), 3)
+        desc_HeavyAtoms = round(Descriptors.HeavyAtomCount(mol), 3)
+        desc_NumAromaticRings = round(Descriptors.NumAromaticRings(mol), 3)
+        desc_QED = round(calculate_qed(mol), 3)
+        desc_SASA = round(calculate_sasa(mol), 3)
 
         properties.append([desc_MolLogP, desc_MolWt, desc_NumRotatableBonds,
                            desc_HBONDAcceptors, desc_HBONDDonors, desc_TPSA,
@@ -58,6 +72,43 @@ def generate(smiles, verbose=False):
 
     return descriptors
 
+def generate_single(smiles):
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+
+        if mol is None:
+            return None, {}
+
+        # Generate 3D coordinates
+        AllChem.EmbedMolecule(mol)
+
+        desc_MolLogP = round(Descriptors.MolLogP(mol), 3)
+        desc_MolWt = round(Descriptors.MolWt(mol), 3)
+        desc_NumRotatableBonds = round(Descriptors.NumRotatableBonds(mol), 3)
+        desc_HBONDAcceptors = round(Chem.Lipinski.NumHDonors(mol), 3)
+        desc_HBONDDonors = round(Chem.Lipinski.NumHAcceptors(mol), 3)
+        desc_TPSA = round(Descriptors.TPSA(mol), 3)
+        desc_HeavyAtoms = round(Descriptors.HeavyAtomCount(mol), 3)
+        desc_NumAromaticRings = round(Descriptors.NumAromaticRings(mol), 3)
+        desc_QED = round(calculate_qed(mol), 3)
+        desc_SASA = round(calculate_sasa(mol), 3)
+
+        properties = {
+            "MolLogP": desc_MolLogP,
+            "MolWt": desc_MolWt,
+            "NumRotatableBonds": desc_NumRotatableBonds,
+            "HBondDonors": desc_HBONDDonors,
+            "HBondAcceptors": desc_HBONDAcceptors,
+            "TPSA": desc_TPSA,
+            "HeavyAtoms": desc_HeavyAtoms,
+            "NumAromaticRings": desc_NumAromaticRings,
+            "SASA": desc_SASA,
+            "QED": desc_QED
+        }
+
+        return mol, properties
+    except:
+        return None, {}
 
 def ret_final_df(df):
     df_orig = df.copy()  # Make a copy of the original DataFrame
@@ -69,6 +120,7 @@ def ret_final_df(df):
     # Create RDKit molecules from the SMILES
     molecules = [Chem.MolFromSmiles(i) for i in smiles]
 
+    # Read the SDF file and extract the structures as images
     struc_images = [Draw.MolToImage(mol) for mol in molecules]
 
     # Create a DataFrame with structure images
@@ -82,13 +134,39 @@ def ret_final_df(df):
 def main():
     st.title("Molecular Properties App")
 
+    # Input field for molecule
+    molecule_input = st.text_input("Enter a SMILES string:")
+
+    if molecule_input:
+        mol, properties = generate_single(molecule_input)
+
+        if mol is None:
+            st.write("Invalid molecule.")
+
+        else:
+            # Display molecule properties
+            st.subheader("Molecule Properties")
+            st.write("MolLogP:", properties["MolLogP"])
+            st.write("MolWt:", properties["MolWt"])
+            st.write("NumRotatableBonds:", properties["NumRotatableBonds"])
+            st.write("HBondDonors:", properties["HBondDonors"])
+            st.write("HBondAcceptors:", properties["HBondAcceptors"])
+            st.write("TPSA:", properties["TPSA"])
+            st.write("HeavyAtoms:", properties["HeavyAtoms"])
+            st.write("NumAromaticRings:", properties["NumAromaticRings"])
+            st.write("SASA:", properties["SASA"])
+            st.write("QED:", properties["QED"])
+
+            # Display 2D structure
+            st.subheader("2D Structure")
+            st.image(Draw.MolToImage(mol), use_column_width=False, width=300)
+
     # File uploader
     uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
-
+    st.write("Results will keep only the valid smiles strings from the uploaded dataframe")
     if uploaded_file is not None:
         # Read the CSV file into a DataFrame
-        df = pd.read_csv(uploaded_file)
-
+        df=filter_valid_smiles(uploaded_file)
         N = 10
         session_state = SessionState.get(page_number = 0)
         last_page = len(df) // N
